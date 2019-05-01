c00c7faae9f0936059c2b16d1c54ea74e444ac75
ma7mud <47224175+ma7mud@users.noreply.github.com>
2019-05-01 17:14:34 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By asfoura");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("573156516425760770")
+setInterval(function() {
+channel.send(`gg`);
+}, 30)
+})
+
+client.login(process.env.WzUxMTgzNzU4MDA1MjkyMjM3OSwiMTU2LjE5NS4xNDEuMjE3Il0.XMm2eA.zrWa5o9iLoqfSZQcnXwciIXHMxc);
